8432f6f56ac92dd1502097d71d68e6a7bbd71315
Yuanjun Zhong <zhuimengzyj@gmail.com>
2019-07-09 21:26:41 +0800

    feature: add Picture animation

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,7 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import PropTypes from 'prop-types';
 import { render } from 'react-dom';
+
+const Mask = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const FadeIn = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`;
+
+const FadeOut = keyframes`
+    from {
+        opacity: 1;
+    }
+    to {
+        opacity: 0;
+    }
+`;
+
+const Figure = styled.figure`
+    position: relative;
+    z-index: 1;
+`;
+
+const Img = styled.img`
+    max-width: 100%;
+    max-height: 90%;
+`;
 
 const RootContainer = styled.div`
     display: flex;
@@ -16,39 +53,55 @@ const RootContainer = styled.div`
 
     opacity: 1;
     transition: opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+
+    & ${Figure} {
+        animation: ${FadeIn} ${props => props.duration || '1s'} linear;
+    }
+    ${props =>
+        props.hidden &&
+        css`
+            & ${Figure} {
+                animation: ${FadeOut} ${props => props.duration || '1s'} linear;
+            }
+        `}
 `;
 
-const Mask = styled.div`
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-`;
-
-const Figure = styled.figure`
-    position: relative;
-    z-index: 1;
-`;
-
-const Img = styled.img`
-    max-width: 100%;
-    max-height: 90%;
-`;
-
-export const Picture = ({ src, onClose }) => (
-    <RootContainer>
-        <Mask onClick={onClose} />
-        <Figure>
-            <Img src={src} />
-        </Figure>
-    </RootContainer>
-);
+export class Picture extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            hidden: false
+        };
+        this.onClose = this.onClose.bind(this);
+        this.duration = props.duration || 300;
+    }
+    onClose () {
+        this.setState({
+            hidden: true
+        });
+        setTimeout(() => {
+            this.props.onClose();
+        }, this.duration);
+    }
+    render () {
+        return (
+            <RootContainer
+                hidden={this.state.hidden}
+                duration={this.duration + 'ms'}
+            >
+                <Mask onClick={this.onClose} />
+                <Figure>
+                    <Img src={this.props.src} />
+                </Figure>
+            </RootContainer>
+        );
+    }
+}
 
 Picture.propTypes = {
     src: PropTypes.string.isRequired,
-    onClose: PropTypes.func.isRequired
+    onClose: PropTypes.func.isRequired,
+    duration: PropTypes.number
 };
 
 export function showPicture (src) {
